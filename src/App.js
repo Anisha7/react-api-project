10795d729314b35b474c6d1c8adda5f473f3a97f
Anisha Jain <anishajain@outlook.com>
2019-02-11 19:57:49 -0800

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import InputZip from './InputZip'
+import InputZip from './InputZip';
 
 import './App.css';
 
@@ -27,29 +27,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weatherdata: null
-    }
+      weatherdata: null,
+    };
   }
 
   submitZip(zip) {
-    console.log(`Zip: ${zip}`)
+    console.log(`Zip: ${zip}`);
     // load weather data
     const apikey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+    console.log(apikey)
+
     // Form an API request URL with the apikey and zip
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apikey}`;
     // Get data from the API with fetch
     fetch(url).then((res) => {
+      console.log("HERE")
       // Handle the response stream as JSON
       return res.json();
     }).then((json) => {
+      console.log("JSON " + JSON.stringify(json))
       // If the request was successful assign the data to component state
-      this.setState({ weatherData: json });
+      this.setState({ weatherdata: json });
       // ! This needs better error checking here or at renderWeather()
       // It's possible to get a valid JSON response that is not weather
       // data, for example when a bad zip code entered.
     }).catch((err) => {
       // If there is no data
-      this.setState({ weatherData: null }); // Clear the weather data we don't have any to display
+      this.setState({ weatherdata: null }); // Clear the weather data we don't have any to display
       // Print an error to the console.
       console.log('-- Error fetching --');
       console.log(err.message);
@@ -60,8 +64,8 @@ class App extends Component {
 
   render() {
     let weatherComponent = (<p> No data available</p>)
-    if (this.weatherData != null) {
-      weatherComponent = (<Weather weatherdata={this.state.weatherdata} />)
+    if (this.weatherdata != null) {
+      weatherComponent = (<Weather weatherdata={this.state.weatherdata} />);
     }
     return (
       <div className="App">
